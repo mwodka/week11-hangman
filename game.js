3f6ca6a493a8ac1b02ec16ee3434d4b774bf7054
mwodka <mrw.simple@gmail.com>
2016-10-21 10:10:27 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,12 @@
 var exports = module.exports = {};
+var wordBank = [
+	'anything',
+	'works',
+	'here',
+	'for',
+	'now'
+];
 module.exports.getRandomWord = function () {
-	var index = Math.random() * (max);
+	var index = Math.random() * (wordBank);
 }
 
